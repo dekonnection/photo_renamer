15a60b20c252b99fe5714b1d734ceb1404cd5648
Cédric Laudrel <dek@iono.me>
2019-07-28 23:43:04 +0200

    fix "too many open files"

--- photo_renamer/__main__.py
+++ photo_renamer/__main__.py
@@ -17,7 +17,6 @@ def parse_args(args=None):
         "-f",
         "--files",
         required=True,
-        type=FileType("rb"),
         nargs="+",
         help="File(s) to rename, separated by spaces",
     )
@@ -27,32 +26,32 @@ def parse_args(args=None):
 def build_rename_dict(file_list):
     rename_dict = {}
     for file in file_list:
-        path = dirname(file.name)
-        extension = splitext(file.name)[1]
+        path = dirname(file)
+        extension = splitext(file)[1]
         new_name = gen_new_name(file)
 
         new_full_path = join(path, new_name + extension)
 
-        rename_dict[file.name] = new_full_path
-        file.close()
+        rename_dict[file] = new_full_path
     return rename_dict
 
 
-def gen_new_name(file):
-    image = Image(file)
-    if not image.has_exif:
-        print(
-            f"ERROR : '{file.name}' is not a supported image, we will exit. Nothing has been renamed."
+def gen_new_name(filepath):
+    with open(filepath, 'rb') as file:
+        image = Image(file)
+        if not image.has_exif:
+            print(
+                f"ERROR : '{file.name}' is not a supported image, we will exit. Nothing has been renamed."
+            )
+            exit(1)
+
+        original_name = splitext(basename(file.name))[0]
+        date_id = datetime.strptime(image.datetime, "%Y:%m:%d %H:%M:%S").strftime(
+            "%Y%m%d-%H%M%S"
         )
-        exit(1)
 
-    original_name = splitext(basename(file.name))[0]
-    date_id = datetime.strptime(image.datetime, "%Y:%m:%d %H:%M:%S").strftime(
-        "%Y%m%d-%H%M%S"
-    )
-
-    new_name = f"{date_id}_{image.model}_{original_name}"
-    return new_name
+        new_name = f"{date_id}_{image.model}_{original_name}"
+        return new_name
 
 
 def massive_rename(rename_dict, dry=False):
@@ -76,6 +75,7 @@ def main():
         "Welcome to photo_renamer !\n(this is unsupported software, please make backups before using it)\n"
     )
 
+    print('Analyzing files ...\n')
     rename_dict = build_rename_dict(args.files)
 
     print("\nYour files would be renamed like this :\n")
